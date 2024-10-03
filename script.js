972b6b8dbd3af1e3833678d0f9887b691e3e269a
Uday_Patel <148987243+Uday12704@users.noreply.github.com>
2024-10-03 10:40:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type",{
-    strings: ["Web develper", "Frontend developer", "Data analyst"],
+    strings: ["Web developer", "Frontend developer", "Data analyst"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
